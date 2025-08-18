f07a973d4cd86d096da8db5dc027d59a9294272a
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-08-18 13:28:51 +0600

    create to the navbar component and work on going

--- src/Pages/Shareds/Footer/Footer.jsx
+++ src/Pages/Shareds/Footer/Footer.jsx
@@ -3,32 +3,6 @@ import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
     <div>
-      {/* <footer className="md:flex ">
-        <aside className=" bg-[#1F2937] py-24 px-44 text-white">
-          <h2 className="uppercase md:text-3xl">Contact us</h2>
-          <p>123 ABS Street,Uni 21,Bangladesh</p>
-          <span>+88 01814082602</span>
-          <br />
-          <span>Mon - Fri: 08:00 am - 22:00 pm</span>
-          <br />
-          <span>Sat - Sun: 10:00 am - 23:00 pm</span>
-        </aside>
-        <nav className=" bg-[#111827] py-24 px-44 text-white">
-          <h6 className="footer-title">Follow US</h6>
-          <p>Join us on social media</p>
-          <div className="grid grid-flow-col gap-4 my-2">
-            <a>
-              <FaFacebookF />
-            </a>
-            <a>
-              <FaInstagram />
-            </a>
-            <a>
-              <FaTwitter />
-            </a>
-          </div>
-        </nav>
-      </footer> */}
       <footer className="text-white">
         {/* Top Section */}
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 ">
