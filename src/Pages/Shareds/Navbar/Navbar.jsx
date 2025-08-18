daee6a9822b1a8b73bdd11d61018d8cb3542eb59
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-08-18 13:56:01 +0600

    create to the new component in banner section

--- src/Pages/Shareds/Navbar/Navbar.jsx
+++ src/Pages/Shareds/Navbar/Navbar.jsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
+const navOptions = [
+  <li>
+    <Link to={"/"}>Home</Link>
+  </li>,
+  <li>
+    <Link to={"contact"}>Contact Us</Link>
+  </li>,
+  <li>
+    <Link to={"dashboard"}>Dashboard</Link>
+  </li>,
+  <li>
+    <Link to={"ourMenu"}>Our Menu</Link>
+  </li>,
+  <li>
+    <Link to={"ourShop"}>Our Shop</Link>
+  </li>,
+];
 const Navbar = () => {
   return (
     <div>
@@ -25,49 +44,13 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {navOptions}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">MRS Restaurant</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal px-1">{navOptions}</ul>
         </div>
         <div className="navbar-end">
           <a className="btn">Button</a>
