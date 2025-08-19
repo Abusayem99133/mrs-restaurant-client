c1d69f581a6ce0a51806ff41297c231d85ecc35d
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-08-19 09:47:07 +0600

    navbar section work done

--- src/Pages/Shareds/Navbar/Navbar.jsx
+++ src/Pages/Shareds/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const navOptions = [
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar max-w-screen-xl  fixed z-10  bg-black/50 text-white">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
