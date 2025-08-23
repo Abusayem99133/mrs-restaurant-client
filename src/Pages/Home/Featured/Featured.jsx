3542e1cb40e51b98b508acdaf87c2a77ec2700a1
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-08-23 15:34:08 +0600

    created to the featured css component and work and customize style css

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -1,13 +1,30 @@
-import React from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
-
+import featuredImg from "../../../assets/home/featured.jpg";
+import SharedButton from "../../Shareds/SharedButton";
+import "../Featured/Featured.css";
 const Featured = () => {
   return (
-    <div>
+    <div className="featured-item text-white">
       <SectionTitle
         subHeading="Check it out"
         heading="Featured Item"
       ></SectionTitle>
+      <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+        <div>
+          <img src={featuredImg} alt="" />
+        </div>
+        <div className="md:ml-10">
+          <p>Aug 29, 2025</p>
+          <p className="uppercase">Where can i get some? </p>
+          <p>
+            You can enjoy a variety of delicious dishes at MRS Restaurant, where
+            quality food meets a cozy atmosphere. Whether you’re dining in,
+            taking away, or ordering online, MRS Restaurant is always ready to
+            serve you with love and care.”
+          </p>
+          <button className="btn btn-outline">Order Now</button>
+        </div>
+      </div>
     </div>
   );
 };
