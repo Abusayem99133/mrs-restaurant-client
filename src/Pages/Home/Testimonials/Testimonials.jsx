0d91f76dd0f9912336e6901f08e7d17b86ad6507
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-08-26 13:01:00 +0600

    update to the testimonials

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,14 +1,32 @@
-import React from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
 
+import "swiper/css";
+import "swiper/css/navigation";
+// import required modules
+import { Navigation } from "swiper/modules";
 const Testimonials = () => {
   return (
-    <section>
+    <section className="my-20">
       <SectionTitle
         subHeading={"What Our Client Say"}
         heading={"Testimonials"}
       />
-      <div>This is Testimonials</div>
+      <div>
+        {" "}
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
     </section>
   );
 };
