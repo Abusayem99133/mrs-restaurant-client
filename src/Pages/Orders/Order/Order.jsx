ccfd20520af39d5a299070babaa7c39a6d9fe7b3
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-09-02 14:38:21 +0600

    install to the react tabs

--- src/Pages/Orders/Order/Order.jsx
+++ src/Pages/Orders/Order/Order.jsx
@@ -1,19 +1,23 @@
 import { useParams } from "react-router-dom";
 import orderCover from "../../../assets/shop/banner2.jpg";
-
 import useMenu from "../../../hooks/useHooks";
 import Cover from "../../Shareds/Cover/Cover";
 import OrderTab from "../OrderTab/OrderTab";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
 const Order = () => {
+  const categories = ["salad", "pizza", "soup", "dessert", "drinks"];
+  const { category } = useParams(); // object destructure korte hobe
+  const initialIndex = categories.indexOf(category);
   const [menu] = useMenu();
-  const { category } = useParams();
-  console.log(category);
+
   const desserts = menu?.filter((item) => item?.category === "dessert");
   const soup = menu?.filter((item) => item?.category === "soup");
   const salad = menu?.filter((item) => item?.category === "salad");
   const pizza = menu?.filter((item) => item?.category === "pizza");
-  // const offered = menu?.filter((item) => item?.category === "offered");
   const drinks = menu?.filter((item) => item?.category === "drinks");
+
   return (
     <div>
       <Cover
@@ -22,57 +26,35 @@ const Order = () => {
         recipe="Choose your favorite dishes from our menu and get them delivered 
   fresh and hot right to your doorstep. Enjoy a seamless ordering experience."
       />
-      {/* name of each tab group should be unique */}
-      <div className="tabs tabs-border">
-        <input
-          type="radio"
-          name="my_tabs_2"
-          className="tab uppercase"
-          aria-label="Salad"
-        />
-        <div className="tab-content border-base-300 bg-base-100 p-10">
+
+      <Tabs
+        defaultIndex={initialIndex}
+        onSelect={(index) => console.log(index)}
+      >
+        <TabList>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soup</Tab>
+          <Tab>Dessert</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+
+        <TabPanel>
           <OrderTab items={salad} />
-        </div>
-
-        <input
-          type="radio"
-          name="my_tabs_2"
-          className="tab uppercase"
-          aria-label="Pizza"
-          defaultChecked
-        />
-        <div className="tab-content border-base-300 bg-base-100 p-10">
+        </TabPanel>
+        <TabPanel>
           <OrderTab items={pizza} />
-        </div>
-
-        <input
-          type="radio"
-          name="my_tabs_2"
-          className="tab uppercase"
-          aria-label="Soups"
-        />
-        <div className="tab-content border-base-300 bg-base-100 p-10">
+        </TabPanel>
+        <TabPanel>
           <OrderTab items={soup} />
-        </div>
-        <input
-          type="radio"
-          name="my_tabs_2"
-          className="tab uppercase"
-          aria-label="Desserts"
-        />
-        <div className="tab-content border-base-300 bg-base-100 p-10">
+        </TabPanel>
+        <TabPanel>
           <OrderTab items={desserts} />
-        </div>
-        <input
-          type="radio"
-          name="my_tabs_2"
-          className="tab uppercase"
-          aria-label="Drinks"
-        />
-        <div className="tab-content border-base-300 bg-base-100 p-10">
+        </TabPanel>
+        <TabPanel>
           <OrderTab items={drinks} />
-        </div>
-      </div>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
