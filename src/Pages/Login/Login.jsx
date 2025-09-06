b83e9546a0233575fe0f3e7eb5fc8072f1d06c42
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-09-06 17:07:10 +0600

    login page ui work ongoing

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,52 @@
 const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col-reverse md:flex-row-reverse">
+        <div className="text-center md:w-1/2 lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 md:w-1/2 max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body">
+            <fieldset className="fieldset">
+              <label className="label">Email</label>
+              <input
+                name="email"
+                type="email"
+                className="input"
+                placeholder="Email"
+              />
+              <label className="label">Password</label>
+              <input
+                name="password"
+                type="password"
+                className="input"
+                placeholder="Password"
+              />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+
+              <input
+                className="btn btn-primary mt-4"
+                type="submit"
+                value={"Login"}
+              />
+            </fieldset>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
