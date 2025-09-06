f63f7872015a863883ce9182c9b73ad97a2c49f8
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-09-06 06:46:53 +0600

    updated to the pagination system

--- src/Pages/Orders/OrderTab/OrderTab.jsx
+++ src/Pages/Orders/OrderTab/OrderTab.jsx
@@ -1,11 +1,30 @@
 import FoodCard from "../../../Components/FoodCard/FoodCard";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
 const OrderTab = ({ items }) => {
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-10">
-      {items?.map((item) => (
-        <FoodCard key={item?._id} item={item}></FoodCard>
-      ))}
+    <div>
+      <Swiper
+        pagination={{
+          type: "fraction",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-10">
+            {items?.map((item) => (
+              <FoodCard key={item?._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
