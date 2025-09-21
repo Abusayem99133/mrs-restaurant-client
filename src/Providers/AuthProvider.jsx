ffd1b15bbe3479a4d22938b68a9834822631b816
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-09-21 17:23:00 +0600

    updated

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -9,7 +9,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 export const AuthContext = createContext(null);
-// firebase auth system
+// firebase auth system update
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
